e483e020f44fa6799c7d1deef0a861738382c6f7
Dadabhau <dadasthete987@gmail.com>
2026-04-20 16:19:09 +0530

    Ng-content added

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from './layouts/header/header';
 import { ServerStatus } from './pages/dashboard/server-status/server-status';
 import { Traffic } from './pages/dashboard/traffic/traffic';
 import { Tickets } from './pages/dashboard/tickets/tickets';
+import { DashboardItem } from './pages/dashboard/dashboard-item/dashboard-item';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, ServerStatus, Traffic, Tickets],
+  imports: [Header, ServerStatus, Traffic, Tickets, DashboardItem],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
