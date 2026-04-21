2e1b22e0c230ff415e591c691b7dab93e2019895
Dadabhau <dadasthete987@gmail.com>
2026-04-21 22:54:25 +0530

    Host and ViewEncapsulation added

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,10 +1,11 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.html',
   styleUrl: './control.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class Control {
   // @Input() label = '';
